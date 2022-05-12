dad779817353da9b5896f55aee61a1bc2550fecc
adeliiman <62771390+adeliiman@users.noreply.github.com>
2022-05-13 01:31:19 +0430

    Update api_key.py

--- api_key.py
+++ api_key.py
@@ -1,10 +1,6 @@
 # spot_api
-key = "625c789ba039360001e657d6"
-secret = "6e9612d2-d9bc-4efe-9a6a-278ba7687eb9" 
-passphrase = "ma3ta399669"
+key = ""
+secret = "" 
+passphrase = ""
 
 
-# sandbox_api
-#key = '62666b0641a5330001d14837'
-#secret = '7e890961-6c74-4c8f-b347-a0cf58c77a7c'
-#passphrase = "ma3ta399669"
